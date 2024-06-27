7c658ab78dd87237ae4d904d238f08f79775bc1b
nexuscrypt <100082538+nexuscrypt@users.noreply.github.com>
2024-06-27 19:39:39 +0000

    -

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -2,12 +2,14 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { createPool } from "@/utils/sundaeSwapUtils";
 
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-  const result = await createPool();
-
-  res.status(200).json(result);
+  try {
+    const result = await createPool();
+    res.status(200).json(result);
+  }
+  catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
 }
